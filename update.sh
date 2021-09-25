a53254ffc2ccd621c9a6349facd4d072507e9d0e
hmq <394047464@qq.com>
2021-09-25 15:07:58 +0800

    update

--- update.sh
+++ update.sh
@@ -4,6 +4,4 @@ git checkout .
 git fetch --all
 git reset --hard origin/master 
 git pull origin master 
-cd QQBot-linux-x64 && chmod 777 QQBot.Web && nohup ./QQBot.Web &
-
-kill -9 $( ps -e|grep go-cqhttp |awk '{print $1}') 
+chmod 777 QQBot.Web && nohup ./QQBot.Web &
